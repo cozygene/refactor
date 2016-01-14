df35fd456c69677d1403b81c63f434d2e2338836
yedidimr <yedidimr@gmail.com>
2016-01-14 15:48:38 +0200

    move refactor.R to refactor_lib.R and make refactor.R an argument parser

--- R/demo.R
+++ R/demo.R
@@ -1,5 +1,5 @@
 
-if(!exists("refactor", mode="function")) source("refactor.R")
+if(!exists("refactor", mode="function")) source("refactor_lib.R")
 
 associations_test <- function(O, y, model_append)
 {
@@ -35,34 +35,30 @@ DATA_FILE = '../demo_files/demo_datafile.txt'             # methylation levels f
 PHENO_FILE = '../demo_files/demo_phenotype.txt'           # phenotype file path
 CELL_COMP_FILE = '../demo_files/demo_cellproportions.txt' # cell composition file path
 
-# args <- commandArgs(trailingOnly = TRUE)
-# data_file=args[1] # data file
-# y=args[2] # a phenotype file
-# K=as.numeric(args[3]) # an integer
-# R=args[4] # a matrix with k columns; these are the cell proportions)
 
+print("Reading methylation data...")
+O = as.matrix(read.table(DATA_FILE))    
+sample_id_O <- O[1, -1] # extract samples ID
+O <- O[-1,] # remove sample ID from matrix
+cpgnames <- O[, 1] ## set rownames
+O <- O[, -1] 
+O = t(matrix(as.numeric(O),nrow=nrow(O),ncol=ncol(O)))
 
-#TODO enter to funs?
-    O = as.matrix(read.table(DATA_FILE))    
-    sample_id_O <- O[1, -1] # extract samples ID
-    O <- O[-1,] # remove sample ID from matrix
-    cpgnames <- O[, 1] ## set rownames
-    O <- O[, -1] 
-    O = t(matrix(as.numeric(O),nrow=nrow(O),ncol=ncol(O)))
-
+print("Reading phenotype data...")
 phenotype_matrix = as.matrix(read.table(PHENO_FILE))
 y <-  matrix(as.numeric(as.matrix(phenotype_matrix[, -1]) ))
 
 
-
-png('plot.png')
-par(mfrow=c(2,2))
-
 # run refactor
-output <- refactor(DATA_FILE, K)
+output <- refactor(DATA_FILE, K, num_components=NUM_COMPONENTS)
 
 # run expirements
 print("Unadjusted analysis...")
+
+# init plot
+png('plot.png')
+par(mfrow=c(2,2))
+
 # exp1
 print("START EXP1")
 observed_pvalues <- associations_test(O,y, matrix(0, nrow = nrow(O), ncol = 1))
