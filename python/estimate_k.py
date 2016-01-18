fe047fc2e2c5d2732353fb3a9258654f13b4a3ac
Elior Rahmani <elior@britanya013-3.tau.ac.il>
2016-01-18 12:46:28 +0200

    estimate_k and estimate_t

---
+++ python/estimate_k.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import argparse
+from sklearn import preprocessing
+from numpy import dot, linalg, sqrt, loadtxt
+import pca
+import refactor_lib
+from methylation_data import MethylationData
+from refactor_lib import Refactor
+
+
+class kEstimateArgumentParser(argparse.ArgumentParser):
+
+    def error(self, message):
+        sys.stderr.write('Error: %s\n' % message)
+        print("To see the full help: %s -h/--help" % self.prog)
+        sys.exit(2)
+
+def run():
+
+        parser = kEstimateArgumentParser(prog=os.path.basename(sys.argv[0]), description = "estimate_k: a tool for estimating the parameter k for ReFACTor by computing a score for the first several eigenvalues of the data matrix. k should be selected to be the number of substantial eigenvalues (in terms of their scores).", epilog = "", add_help=False)
+        required = parser.add_argument_group('required arguments')
+        required.add_argument('--datafile', required = True, type = str, help = "A data matrix file of beta-normalized methylation levels; see the instructions of refactor.py for more details")
+      
+        optional = parser.add_argument_group('optional arguments')
+        optional.add_argument('-h', '--help', action='help')
+        optional.add_argument('--max_k', type = int, default = 10, help = "The maximal eigenvalue to display (DEFAULT=10)")
+              
+        args = parser.parse_args()
+
+        # load methylation data file
+        meth_data = MethylationData(datafile = args.datafile)
+        n = meth_data.data.shape[1] # number of samples
+        
+        # Validate input
+        if (args.max_k < 3 or args.max_k > n-1):
+            print("max_k must be greater than 2 and smaller than the number of samples")
+            sys.exit(2)
+        
+        Refactor.estimate_k(methylation_data = meth_data, max_k = args.max_k)
+        
+
+if __name__ == "__main__":
+  run()
---
+++ python/estimate_t.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import argparse
+from sklearn import preprocessing
+from numpy import dot, linalg, sqrt, loadtxt
+import pca
+import refactor_lib
+from methylation_data import MethylationData
+from refactor_lib import Refactor
+
+
+class tEstimateArgumentParser(argparse.ArgumentParser):
+
+    def error(self, message):
+        sys.stderr.write('Error: %s\n' % message)
+        print("To see the full help: %s -h/--help" % self.prog)
+        sys.exit(2)
+
+def run():
+        parser = tEstimateArgumentParser(prog=os.path.basename(sys.argv[0]), description = "estimate_t: a tool for estimating the parameter t for ReFACTor, by computing a score for the sites that were best approximated by a low-rank approximation. t should be selected to be the number of sites with substantial signal.", epilog = "", add_help=False)
+        required = parser.add_argument_group('required arguments')
+        required.add_argument('--datafile', required = True, type = str, help = "A data matrix file of beta-normalized methylation levels; see the instructions of refactor.py for more details")
+        required.add_argument('--k', required = True, type = int, help = "The number of assumed cell types")
+      
+        optional = parser.add_argument_group('optional arguments')
+        optional.add_argument('-h', '--help', action='help')
+        optional.add_argument('--numsites', type = int, default = 5000, help = "The number of sites to display (DEFAULT=min{5000,#sites})")
+              
+        args = parser.parse_args()
+
+        # load methylation data file
+        meth_data = MethylationData(datafile = args.datafile)
+
+        m = meth_data.data.shape[0] # number of sites
+        if (m < args.numsites):
+            args.numsites = m
+
+        # Validate input
+        if (args.numsites < 1):
+            print("numsites must be greater than 0")
+            sys.exit(2)
+
+        Refactor.estimate_t(methylation_data = meth_data, k = args.k, numsites = args.numsites)
+        
+
+if __name__ == "__main__":
+  run()
+    
+
