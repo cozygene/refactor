b0f2d4b0d757fec855c748fc231be10e4a9587bb
yedidimr <yedidimr@gmail.com>
2016-03-16 16:32:48 +0200

    Support installation on windows and colored prints

--- python/install.py
+++ python/install.py
@@ -4,13 +4,104 @@ import os
 import sys
 from setuptools import setup
 
+
+
+if os.name == 'nt':
+        
+    class FOREGROUND:
+        BLACK     = 0x0000
+        BLUE      = 0x0001
+        GREEN     = 0x0002
+        CYAN      = 0x0003
+        RED       = 0x0004
+        PURPLE    = 0x0005
+        YELLOW    = 0x0006
+        WHITE     = 0x0007
+        GREY      = 0x0008
+
+    class BACKGROUND:
+        BLACK     = 0x0000
+        BLUE      = 0x0010
+        GREEN     = 0x0020
+        CYAN      = 0x0030
+        RED       = 0x0040
+        PURPLE    = 0x0050
+        YELLOW    = 0x0060
+        GREY      = 0x0080
+
+    import ctypes
+    STD_OUTPUT_HANDLE = ctypes.windll.kernel32.GetStdHandle(-11) #windows stdout device 
+
+    def get_csbi_attributes(handle):
+        import struct
+        csbi = ctypes.create_string_buffer(22)
+        res = ctypes.windll.kernel32.GetConsoleScreenBufferInfo(handle, csbi)
+        assert res
+        (_, _, _, _, wattr, _, _, _, _, _, _) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+        return wattr
+
+    RESET = get_csbi_attributes(STD_OUTPUT_HANDLE)
+
+
+elif os.name == 'posix':
+    class FOREGROUND:
+        BLACK     = '\033[30m'
+        BLUE      = '\033[94m'
+        GREEN     = '\033[92m'
+        CYAN      = '\033[96m'
+        RED       = '\033[91m'
+        PURPLE    = '\033[35m'
+        YELLOW    = '\033[33m'
+        WHITE     = '\033[37m'
+        GREY      = '\033[90m'
+               
+    class BACKGROUND:
+        BLACK     = '\033[40m'
+        BLUE      = '\033[44m'
+        GREEN     = '\033[42m'
+        CYAN      = '\033[46m'
+        RED       = '\033[41m'
+        PURPLE    = '\033[45m'
+        YELLOW    = '\033[43m'
+        GREY      = '\033[47m'
+
+    RESET ='\033[0m'
+    
+    # # unix styles :
+    #     bold='\033[01m'
+    #     disable='\033[02m'
+    #     underline='\033[04m'
+    #     reverse='\033[07m'
+    #     strikethrough='\033[09m'
+    #     invisible='\033[08m'
+
+
+
+    
 REFACTOR_OBLIGATORY_DEPENDENCIES = ['numpy', 'scipy', 'sklearn']
 REFACTOR_OPTIONAL_DEPENDENCIES = ['matplotlib','statsmodels'] # for demo only
 
 def setup_refactor():
+    """
+    adds refactor.py to PATH so it can be run from anywhere
+    NOTE: on windows: refactor.py is added to C:\PythonXX\Scripts
+          but this folder is not on PATH by default and user must add it so he can run refactor.py from anywhere
+    """
     # add "install" command for the setup script. "mimic" user choice: setup install 
     sys.argv.append("install")
-    print sys.argv
+    
+
+    # TODO: add  C:\PythonXX\Scripts to registry or environment in a way it will stay there after script terminates
+    # could be something like that:
+    #
+    # aKey = _winreg.OpenKey(aReg, r"SYSTEM\CurrentControlSet\Control\Session Manager\Environment", 0, _winreg.KEY_SET_VALUE)
+    # try:   
+    #     _winreg.SetValueEx(key = aKey,value_name="Path",type=_winreg.REG_EXPAND_SZ, value= os.path.join(os.path.dirname(sys.executable), "Scripts") ) 
+    # except EnvironmentError:                                          
+    #     print "Encountered problems writing into the Registry..."
+    # _winreg.CloseKey(aKey)
+    # _winreg.CloseKey(aReg)        
+    
     setup(name='refactor',
         version='1.0',
         description='',
@@ -20,7 +111,7 @@ def setup_refactor():
         #install_requires=[], # leave this commented out.declaring dependencies that are already install can cause problems
         include_package_data=True,
         zip_safe=False,
-)
+    )
 
 def run_function_without_prints(func):
     """
@@ -78,13 +169,13 @@ def install(module_name):
     if exit_code == 0: # success
         try:
             __import__(module_name)
-            print "Package %s installed successfully!" % module_name
+            color_print("Package %s installed successfully!" % module_name, FOREGROUND.GREEN)
             return True
             
-        except Exception, e:
+        except Exception as e:
             pass
 
-    print "Package %s installation failed" % module_name
+    color_print("Package %s auto-installation FAILED" % module_name, FOREGROUND.RED)
     return False
 
 
@@ -150,7 +241,7 @@ def install_refactor():
     dependencies_to_install = check_dependencies(REFACTOR_OBLIGATORY_DEPENDENCIES + REFACTOR_OPTIONAL_DEPENDENCIES)
 
     if len(dependencies_to_install) == 0:
-        print "All dependencies are installed! Done."
+        color_print("All dependencies are installed! Done.", FOREGROUND.GREEN)
     else:
         obligatory_dependencies = []
         optional_dependencied = []
@@ -161,23 +252,61 @@ def install_refactor():
                 optional_dependencied.append(module)
         
         if obligatory_dependencies:
-            print "To run ReFACTOR you must install the following packages: %s" % str(obligatory_dependencies)
+            color_print("To run ReFACTor you must install the following packages: %s" % str(obligatory_dependencies), BACKGROUND.CYAN + FOREGROUND.RED)
         if optional_dependencied:
-            print "To run ReFACTOR's demo you need to install: %s " % str(optional_dependencied)
+            color_print("To run ReFACTor's demo.py you need to install: %s " % str(optional_dependencied), BACKGROUND.CYAN + FOREGROUND.RED)
 
-        raw_input("Press any key to continue")
+def isUserAdmin():
+
+    # check for root on Windows
+    if os.name == 'nt':
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            print("Admin check failed, assuming not an admin.")
+            return False
+    # check for root on Posix
+    elif os.name == 'posix':
+        return os.getuid() == 0
+    else:
+        raw_input("Make sure you're running this as Administrator user. Press any key to continue...")
+        return True
         
+
+def color_print(string, color):
+    """
+    prints 'string' with color 'color'
+    NOTE: both on Unix and Windows if you want to add FOREGROUND and BACKGROUND just add them by "+"
+          e.g: FOREGROUND.GREEN + BACKGROUND.YELLOW
+    """
+    if os.name == 'nt':
+        ctypes.windll.kernel32.SetConsoleTextAttribute(STD_OUTPUT_HANDLE, color)
+        print(string)
+        ctypes.windll.kernel32.SetConsoleTextAttribute(STD_OUTPUT_HANDLE, RESET)
+    elif os.name == 'posix':
+        print(color + string + RESET)
+    # color_print on other OS is not supported
+    else:
+        print(string)
 
 if __name__ == '__main__':
 
     # run as root (root privilege is required to run python setup.py script and pip install)
-    euid = os.geteuid()
-    if euid != 0:
-        print "Installation must be started as root. Running sudo..."
-        args = ['sudo', sys.executable] + sys.argv + [os.environ]
-        # the next line replaces the currently-running process with the sudo
-        os.execlpe('sudo', *args)
-
+    if not isUserAdmin():
+        # on Windows
+        if os.name == 'nt':
+            # TODO: make sure if windows require Admin, if so find a way to change to admin and print a message
+            # I think those lines are not working. but should do something like that
+            # import subprocess
+            # subprocess.call(['runas', '/noprofile', '/user:Administrator' ," ".join(sys.argv)])
+            pass
+        # on unix    
+        elif os.name == 'posix':
+            print "Installation must be started as root, Running sudo..."
+            args = ['sudo', sys.executable] + sys.argv + [os.environ]
+            # the next line replaces the currently-running process with the sudo, won't work the same way on windows
+            os.execlpe('sudo', *args)
     
     install_refactor()
+    raw_input("\nPress any key to continue")
 	
